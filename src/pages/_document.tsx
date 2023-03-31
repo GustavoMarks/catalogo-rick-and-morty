7baa4d3d186dabad0d251b728a22c3c8319d9177
Gustavo Marques <gustavosmarquesf@gmail.com>
2023-03-31 18:04:07 -0300

    build: setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <title>Catálogo Rick and Morty</title>
+        <meta name="description" content="Catálogo com lista de personagens da série animada Rick and Morty" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
