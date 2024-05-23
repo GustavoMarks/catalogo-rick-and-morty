cdf7cbe78d5f08207b1b8bf76cdf18a4907b0c97
Gustavo Marques <gustavosmarquesf@gmail.com>
2024-05-23 20:05:17 -0300

    chore: Set dev tools

--- src/components/layout/MainContainer.tsx
+++ src/components/layout/MainContainer.tsx
@@ -1,18 +1,19 @@
-import { Box } from "@mui/material";
-import { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
-export default function MainContainer({ children } : { children: ReactNode }) {
+import { Box } from '@mui/material';
+
+export default function MainContainer({ children }: { children: ReactNode }) {
 	return (
 		<Box
 			component='main'
 			sx={{
 				margin: 0,
-        padding: 0,
-        minHeight: '95vh',
+				padding: 0,
+				minHeight: '95vh',
 				backgroundColor: (theme) => theme.palette.primary.dark,
 			}}
 		>
-			{ children }
+			{children}
 		</Box>
-	)
+	);
 }
